c84b41b859f0a57174d23a8d664e9ad01b5eb417
mukhutis <subhabrata.mukhuti@gmail.com>
2016-05-01 20:26:28 +0530

    Simple change

--- MobileTileUI5/WebContent/mobiletileui5/TileView.controller.js
+++ MobileTileUI5/WebContent/mobiletileui5/TileView.controller.js
@@ -6,6 +6,7 @@ sap.ui.controller("mobiletileui5.TileView", {
 * @memberOf mobiletileui5.TileView
 */
 	onInit: function() {
+		//define the model path
 		var sURI= "http://services.odata.org/Northwind/Northwind.svc/"
 		if (typeof baseURL === "string") 
 			sURI=baseURL; //if mock service use the baseURL
